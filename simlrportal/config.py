32675b27c7dfea0b97855c68d98a080693810ef3
haoda <haoda.li@mail.utoronto.ca>
2019-11-19 21:42:57 -0500

    updae structure

--- simlrportal/config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-    UPLOAD_FOLDER = './datasets/'
-    JSON_AS_ASCII = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
