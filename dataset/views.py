17a7301262e182b4cff86f9f7e40f5ef6d29f3c8
yurina! <32067939+lihd1003@users.noreply.github.com>
2020-02-04 23:22:06 -0500

    update file upload

--- dataset/views.py
+++ dataset/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse, Http404, HttpResponseForbidden, HttpResponseBadRequest
-from settings.settings import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
-from .models import DataFile
+import importlib
+import os
+from shutil import rmtree, unpack_archive, get_archive_formats
 from time import time
 
-from shutil import rmtree, unpack_archive, get_archive_formats
-import os
+from django.http import JsonResponse, Http404, HttpResponseForbidden, HttpResponseBadRequest
+from django.shortcuts import render
+
+from settings.settings import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
+from .models import DataFile
 
 
 def render_dataset(request):
@@ -13,7 +15,7 @@ def render_dataset(request):
 
 
 def render_dataupload(request):
-    return render(request, "dataset/dataupload.html",
+    return render(request, "dataset/dataupload2.html",
                   {'allowed_file': ", ".join(ALLOWED_EXTENSIONS)})
 
 
@@ -41,30 +43,55 @@ def rest_datasets(request):
 
 
 def data_upload(request):
+
     file = request.FILES.get('file', None)
     if not file:
         return HttpResponseBadRequest
+
     file_ori, ext = file.name.rsplit('.', 1)
     ext = ext.lower()
-    hash_name = file_ori + "_" + hex(int(time()))
+    hash_name = file_ori + "_" + hex(int(time()))[2:]
     path = os.path.join(UPLOAD_FOLDER, hash_name + "." + ext)
 
     with open(path, 'wb+') as f:
         for chunk in file.chunks():
             f.write(chunk)
-
     if ext in [x[0] for x in get_archive_formats()]:
         unpack_archive(path, os.path.join(UPLOAD_FOLDER))
         os.rename(os.path.join(UPLOAD_FOLDER, file_ori),
                   os.path.join(UPLOAD_FOLDER, hash_name))
         os.remove(path)
         path = os.path.join(UPLOAD_FOLDER, hash_name)
+    package = request.POST.get("package")
+    method = request.POST.get("method")
+
+    if not method or not package:
+        return HttpResponseBadRequest
+    try:
+        module = importlib.import_module(package)
+        components = method.split(".")
+        for attr in components:
+            module = getattr(module, attr)
+        annData = module(path)
+        if os.path.isdir(path):
+            rmtree(path)
+        else:
+            os.remove(path)
+        path = os.path.join(UPLOAD_FOLDER, hash_name + ".h5ad")
+        annData.write(path)
+    except Exception as e:
+        if path:
+            if os.path.isdir(path):
+                rmtree(path)
+            else:
+                os.remove(path)
+        return JsonResponse({'status': False, 'info': 'Internal Error: ' + str(e)})
 
     saved_file = DataFile(
-        source = request.POST.get("owner", "Upload"),
-        name = request.POST.get("name", "uploaded-file"),
-        path = path,
-        description = request.POST.get("description", "")
+        source=request.POST.get("owner", "Upload"),
+        name=request.POST.get("name", "uploaded-file"),
+        path=path,
+        description=request.POST.get("description", "")
     )
     saved_file.save()
-    return JsonResponse({'name': saved_file.name})
+    return JsonResponse({'status': True, 'info': "File successfully uploaded as " + saved_file.name + ".h5ad"})
